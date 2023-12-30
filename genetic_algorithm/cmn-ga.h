3cb00dc367bae0aa83d2ab443d66a1e6561e32bc
Semyon Burov <ilionsd@gmail.com>
2023-12-30 23:14:50 +0300

    added convergence spopping criteria

--- genetic_algorithm/cmn-ga.h
+++ genetic_algorithm/cmn-ga.h
@@ -18,32 +18,32 @@
 
 namespace genetic_algorithm {
 
-template<class Generator, typename CharT>
+template<class Generator>
 class cmn_optimizer {
 public:
     cmn_optimizer(
         const std::size_t initialPopulation,
+        const std::size_t maxPopulation,
 	    const std::size_t crossoversPerGeneration,
 	    const std::size_t mutationsPerGeneration,
 	    const int nMin,
 	    const int nCrowd,
 	    const int nTry,
-	    const std::size_t maxPopulation,
-        std::random_device& rd, std::basic_ostream<CharT>& logger) 
+	    const double targetClosestAvg,
+        std::random_device& rd) 
     : mInitialPopulation(initialPopulation)
+    , mMaxPopulation(maxPopulation)
     , mCrossoversPerGeneration(crossoversPerGeneration)
     , mMutationsPerGeneration(mutationsPerGeneration)
     , mMin(nMin)
     , mCrowd(nCrowd)
     , mTry(nTry)
-    , mMaxPopulation(maxPopulation)
     , mGenerator(rd)
-    , mLog(logger)
     {}
     ~cmn_optimizer() = default;
 
-    template<typename T1, typename T2>
-    auto optimize(const space<T1, T2>& space, const std::function<T1(const std::valarray<T1>&)> fitness) {
+    template<typename T1, typename T2, class OnNextGeneration>
+    auto optimize(const space<T1, T2>& space, const std::function<T1(const std::valarray<T1>&)> fitness, OnNextGeneration onNextGeneration) {
         // used to allocate memory
         const std::size_t increaseCapacity = (mInitialPopulation > mCrossoversPerGeneration + mMutationsPerGeneration) 
             ? mInitialPopulation
@@ -65,7 +65,7 @@ public:
         memoryUsage += sizeof(std::vector<std::valarray<T2>>) 
             + increaseCapacity * (sizeof(std::valarray<T2>) + space.dimensions * sizeof(T2));
         // Fitness
-        memoryUsage += 5 * (sizeof(std::vector<T1>) + populationCapacity * sizeof(T1));
+        memoryUsage += 6 * (sizeof(std::vector<T1>) + populationCapacity * sizeof(T1));
         // Utility
         memoryUsage += sizeof(std::vector<double>) + populationCapacity * sizeof(double);
         memoryUsage += sizeof(std::vector<bool>) 
@@ -89,6 +89,7 @@ public:
         std::vector<T1> 
             // destructible copy of data for std::nth_element
             destructibleDataCopy(populationCapacity),
+            temporaryDataCopy(populationCapacity),
             // stores fitness function evaluations
             fitnessValues(populationCapacity),
             fitnessScaled(populationCapacity),
@@ -107,7 +108,6 @@ public:
         mGeneration = 0;
 
         while (increaseSize + populationSize < mMaxPopulation) {
-            ++mGeneration;
             //-- Adding new individuals to population --
             for (std::size_t k = 0; k < increase.size(); ++k) {
 
@@ -120,6 +120,14 @@ public:
             populationSize += increaseSize;
             increaseSize = 0;
 
+            onNextGeneration(
+                const_cast<const std::size_t>(mGeneration), 
+                const_cast<const std::size_t>(populationSize),
+                const_cast<const std::vector<std::valarray<T1>>&>(populationReal),
+                const_cast<const std::vector<T1>&>(fitnessValues)
+            );
+            ++mGeneration;
+
             //-- Finding local optima --
             {
                 auto begin = mask.begin();
@@ -131,7 +139,7 @@ public:
                     continue;
                 }
                 for (std::size_t j = 0; j < populationSize; ++j) {
-                    destructibleDataCopy[j] = euclidean_distance(populationReal[i], populationReal[j]);
+                    temporaryDataCopy[j] = destructibleDataCopy[j] = euclidean_distance(populationReal[i], populationReal[j]);
                 }
                 {
                     auto begin = destructibleDataCopy.begin();
@@ -141,7 +149,7 @@ public:
                 {
                     auto locality = destructibleDataCopy[mMin];
                     for (std::size_t j = 0; mask[i] && j < populationSize; ++j) {
-                        auto isNonLocal = (i == j) || (distance[i][j] > locality);
+                        auto isNonLocal = (i == j) || (temporaryDataCopy[j] > locality);
                         mask[i] &&= !isNonLocal || (fitnessValues[i] > fitnessValues[j]);
                         mask[j] &&= isNonLocal || (fitnessValues[i] < fitnessValues[j]);
                     }
@@ -149,12 +157,27 @@ public:
             }
 
             //-- Stopping criteria --
+            auto closestAvg = 0.0;
             {
+                std::size_t localOptimaNumber = 0;
+                auto accumulator = 0.0;
                 for (std::size_t i = 0; i < populationSize; ++i) {
                     if (mask[i]) {
-
+                        ++localOptimaNumber;
+                        auto closest = std::numeric_limits<double>::max();
+                        for (std::size_t j = 0; j < populationSize; ++j) {
+                            auto distance = euclidean_distance(populationMesh[i], populationMesh[j]);
+                            if (i != j && closest > distance) {
+                                closest = distance;
+                            }
+                        }
+                        accumulator += closest;
                     }
                 }
+                closestAvg = accumulator / localOptimaNumber;
+            }
+            if (closestAvg < mTargetClosestAvg) {
+                break;
             }
 
             //-- Calculating adjusted fitness --
@@ -357,41 +380,41 @@ protected:
 
 private:
     const std::size_t mInitialPopulation;
+    const std::size_t mMaxPopulation;
 	const std::size_t mCrossoversPerGeneration;
 	const std::size_t mMutationsPerGeneration;
 	const int mMin;
 	const int mCrowd;
 	const int mTry;
-	const std::size_t mMaxPopulation;
-	const std::uint32_t mPrecision;
-
-    mutable std::basic_ostream<CharT> mLog;
+	const double mTargetClosestAvg;
 
     Generator mGenerator;
     std::uint64_t mGeneration;
 };
 
 struct cmn_parameters {
-    template<class Generator, typename CharT>
-    auto make_optimizer(std::random_device& rd, std::basic_ostream<CharT>& logger) const {
-        return cmn_optimizer<Generator, CharT>(
+    template<class Generator>
+    auto make_optimizer(std::random_device& randomDevice) const {
+        return cmn_optimizer<Generator>(
             initialPopulation,
+            maxPopulation,
             crossoversPerGeneration,
             mutationsPerGeneration,
             nMin, nCrowd, nTry,
-            maxPopulation,
-            rd, logger
+            targetClosestAvg,
+            randomDevice
         );
     }
 
     std::size_t initialPopulation;
+    std::size_t maxPopulation;
 	std::size_t crossoversPerGeneration;
 	std::size_t mutationsPerGeneration;
 	int nMin;
 	int nCrowd;
 	int nTry;
-	std::size_t maxPopulation;
-	std::uint32_t precision;
+	
+	double targetClosestAvg;
 };
 
 template<typename CharT>
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <iostream>
 #include <random>
 
@@ -5,9 +6,55 @@
 #include "genetic_algorithm/cmn-ga.h"
 
 auto main() -> int {
+
+    auto f1 = [](const std::valarray<double>& x) -> double {
+        auto val = std::sin(5.1 * M_PI * x[0] + 0.5);
+        return std::pow(val, 6);
+    };
+    auto f2 = [f1](const std::valarray<double>& x) -> double {
+        auto val = 4.0 * std::log(2.0) * std::pow(x[0] - 0.0667, 2) / 0.64;
+        return std::exp(-val) * f1(x);
+    };
+    std::array<std::valarray<int>, 25> A;
+    for (std::size_t i = 0; i < 5; ++i) {
+        for (std::size_t j = 0; j < 5; ++j) {
+            A[i * 5 + j] = std::move(std::valarray<int>(i - 2, j - 2) * 16);
+        }
+    }
+    auto f3 = [A](const std::valarray<double>& x) -> double {
+        auto val = 0.0;
+        for (std::size_t k = 0; k < A.size(); ++k) {
+            auto denominator = static_cast<double>(k);
+            for (std::size_t dim = 0; dim < x.size(); ++dim) {
+                denominator += std::pow(x[dim] - A[k][dim], 6);
+            }
+            val += 1 / denominator;
+        }
+        return val;
+    };
+    std::array<std::valarray<double>, 5> B = {
+        std::valarray<double>{-20, -20, 0.4, 0.02},
+        std::valarray<double>{-5, -25, 0.2, 0.5},
+        std::valarray<double>{0, 30, 0.7, 0.01},
+        std::valarray<double>{30, 0, 1.0, 2.0},
+        std::valarray<double>{30, -30, 0.05, 0.1}
+    };
+    auto f4 = [B](const std::valarray<double>& x) -> double {
+        auto val = 0.0;
+        auto dims = x.size();
+        for (std::size_t k = 0; k < B.size(); ++k) {
+            auto denominator = 1.0;
+            for (std::size_t dim = 0; dim < x.size(); ++dim) {
+                denominator += std::pow(x[dim] - B[k][dim], 2);
+            }
+            val += B[k][dims] / (denominator * B[k][dims + 1]);
+        }
+        return val;
+    };
+
     std::random_device rd;
     auto params = ::genetic_algorithm::cmn_parameters {};
-    auto cmnGA = params.make_optimizer<std::mt19937>(rd, std::cout);
+    auto cmnGA = params.make_optimizer<std::mt19937>(rd);
 
     auto resolution = std::vector<std::uint32_t>{ 1000, 1000 };
     auto bounds = std::vector<std::pair<const double, const double>>{
@@ -21,7 +68,30 @@ auto main() -> int {
         }
     );
 
-    auto [real, vals] = cmnGA.optimize(space, fitness);
-    
+    auto onNextGeneration = [](std::size_t gen, std::size_t size, const std::vector<std::valarray<double>>& population, const std::vector<double>& fitness) {
+        if (gen % 10 == 0) {
+            std::cout << "\nGeneration " << gen << ", population size " << size;
+            for (std::size_t k = 0; k < size; ++k) {
+                std::cout << "\n" << population[k] << " -> " << fitness[k];
+            }
+            std::cout << std::endl;
+        }
+    };
+
+    auto [real, vals] = cmnGA.optimize(space, fitness, onNextGeneration);
+    std::cout << "\nFinal population size: " << real.size();
+    for (std::size_t k = 0; k < real.size(); ++k) {
+        std::cout << "\n" << real[k] << " -> " << vals[k];
+    }
+    std::cout << std::endl;
     return 0;
+}
+
+template<typename CharT, typename T>
+std::basic_ostream<CharT>& operator<<(std::basic_ostream<CharT>& os, const std::valarray<T>& val) {
+    os << "[ ";
+    for (std::size_t k = 0; k < val.size(); ++k) {
+        os << val[k] << " ";
+    }
+    return os << "]";
 }
